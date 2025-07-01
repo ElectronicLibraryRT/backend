395afc8ec7a6aa5706f1d8518b5235a631f00026
D3nisPi <d3nis.p1shchik@gmail.com>
2025-07-01 18:28:41 +0300

    Создание глобального обработчика исключений

---
+++ src/main/java/com/elibrary/backend/GlobalExceptionHandler.java
@@ -0,0 +1,70 @@
+package com.elibrary.backend;
+
+import com.elibrary.backend.shared.dto.ErrorDTO;
+import com.elibrary.backend.shared.exceptions.ResourceNotFoundException;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDTO> handleAuthorNotFound(ResourceNotFoundException ex) {
+        Map<String, Object> errorDetails = Map.of(
+            "resource", ex.getResourceName(),
+            "field", ex.getFieldName(),
+            "value", ex.getFieldValue()
+        );
+
+        ErrorDTO error = new ErrorDTO(
+            "RESOURCE_NOT_FOUND",
+            ex.getMessage(),
+            errorDetails
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorDTO> handleValidationException(ConstraintViolationException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        for (ConstraintViolation<?> violation : ex.getConstraintViolations()) {
+            String propertyPath = violation.getPropertyPath().toString();
+            String field = propertyPath.substring(propertyPath.lastIndexOf('.') + 1);
+            errors.put(field, violation.getMessage());
+        }
+
+        ErrorDTO error = new ErrorDTO(
+            "VALIDATION_ERROR",
+            "Validation error of path parameters or body",
+            errors
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorDTO> handleTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        String error = String.format("Invalid value '%s' for param '%s'",
+            ex.getValue(),
+            ex.getName());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(new ErrorDTO(
+                "TYPE_MISMATCH",
+                error,
+                Map.of(
+                    "parameter", ex.getName(),
+                    "value", ex.getValue() != null ? ex.getValue().toString() : "null",
+                    "requiredType", ex.getRequiredType() != null ? ex.getRequiredType().getSimpleName() : "Unknown"
+                )
+            ));
+    }
+}
