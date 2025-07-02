a52fcf3a1ff4f610ba9c6428afe03afc6ee73b54
D3nisPi <d3nis.p1shchik@gmail.com>
2025-07-02 20:08:49 +0300

    Добавлены модели Extension, BookLocation (ассоциативная таблица)

---
+++ src/main/java/com/elibrary/backend/booklocation/BookLocation.java
@@ -0,0 +1,33 @@
+package com.elibrary.backend.booklocation;
+
+import com.elibrary.backend.book.Book;
+import com.elibrary.backend.booklocation.id.BookLocationId;
+import com.elibrary.backend.extensions.Extension;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "books_locations")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BookLocation {
+    @EmbeddedId
+    private BookLocationId id;
+
+    @Column(nullable = false, length = 256)
+    private String location;
+
+    @ManyToOne
+    @MapsId("bookId")
+    @JoinColumn(name = "book_id", insertable = false, updatable = false)
+    private Book book;
+
+    @ManyToOne
+    @MapsId("extensionId")
+    @JoinColumn(name = "extension_id", insertable = false, updatable = false)
+    private Extension extension;
+}
